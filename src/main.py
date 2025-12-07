f305a304d1b4dd8b46642e8f30b18106cac95616
Syphi <denys.ralko@preply.com>
2025-12-07 18:01:43 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+"""
+Courses: https://www.csosvita.com/courses/database-internals
+NOTION: https://www.notion.so/CS-Osvita-Database-Internals-Nov-24-Mar-02-2b4e34ccb1ca818d93d7d28880a5d0b8?pvs=9
+"""
+
